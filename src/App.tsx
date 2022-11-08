828b0bd4909572905ef6c47d880de40fa2b0181f
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-07 19:14:24 -0700

    Working on useState & incorporating TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UseStateBasics from "./tutorial/1-useState/setup/3-useState-array";
+
+function App() {
+  return (
+    <div className="container">
+      <UseStateBasics />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tutorial/1-useState/setup/3-useState-array.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { data } from "../../../data";
+
+type People = {
+  id: number;
+  name: string;
+};
+
+const UseStateArray = () => {
+  const [people, setPeople] = useState<People[]>([data]);
+  console.log(setPeople);
+  console.log(people);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div>
+            <h1>{id}</h1>
+            <h2>{name}</h2>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default UseStateArray;
