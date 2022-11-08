39a867b7f00379121ae4494616b27433df2b6ae7
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-07 20:20:06 -0700

    Added item removal btn that updates state #2 fixed merge conflict

--- src/tutorial/1-useState/setup/3-useState-array.tsx
+++ src/tutorial/1-useState/setup/3-useState-array.tsx
@@ -7,17 +7,25 @@ type People = {
 };
 
 const UseStateArray = () => {
-  const [people, setPeople] = useState<People[]>([data]);
+  const [people, setPeople] = useState<People[]>(data);
   console.log(setPeople);
   console.log(people);
+
+  const deleteName = (id: number) => {
+    const updateList = people.filter((person) => person.id !== id);
+    setPeople(updateList);
+  };
+
   return (
     <>
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div>
-            <h1>{id}</h1>
-            <h2>{name}</h2>
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button type="button" onClick={() => deleteName(id)}>
+              remove
+            </button>
           </div>
         );
       })}
