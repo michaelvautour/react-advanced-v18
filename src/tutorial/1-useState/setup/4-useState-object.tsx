80feba49715cfbf3db736924f42c309f232db6fc
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-08 19:17:56 -0700

    Spread Operator to update state of one attri in a multiple value state

---
+++ src/tutorial/1-useState/setup/4-useState-object.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const { name, age, message } = person;
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: "Whoa you did it!" });
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
+};
+
+export default UseStateObject;
