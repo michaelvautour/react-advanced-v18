2b6b835f51031ae6f5df5cc73d5e9c3915991b2c
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-09 20:07:54 -0700

    More complex function that shows why previous state can capture value when a timer/other function is introduced

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UseStateBasics from "./tutorial/1-useState/setup/5-useState-counter";
+import UseStateBasics from "./tutorial/1-useState/setup/5a-useStateComplexCounter";
 
 function App() {
   return (
---
+++ src/tutorial/1-useState/setup/5a-useStateComplexCounter.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const UseStateCounter = () => {
+  const [value, setValue] = useState(0);
+
+  const counterIncrease = () => {
+    setValue(value + 1);
+  };
+
+  const counterDecrease = () => {
+    if (value !== 0) {
+      setValue(value - 1);
+    }
+  };
+
+  const counterReset = () => {
+    setValue(0);
+  };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //   setValue(value + 1);
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" type="button" onClick={() => counterIncrease()}>
+          Increase value
+        </button>
+        <button className="btn" type="button" onClick={() => counterDecrease()}>
+          Decrease Value
+        </button>
+        <button className="btn" type="button" onClick={() => counterReset()}>
+          Reset
+        </button>
+      </section>
+
+      <section style={{ margin: "4rem 0" }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          Increase later
+        </button>
+      </section>
+    </>
+  );
+};
+
+export default UseStateCounter;
