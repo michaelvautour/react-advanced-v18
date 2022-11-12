21a8c9f4f7e0d6ec954776620f50b7c8a08d788f
Michael Vautour <michael.e.vautour@gmail.com>
2022-11-12 10:44:44 -0700

    Terinary operator examples

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.tsx
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.tsx
@@ -4,12 +4,16 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("Mike &");
-
+  const [isError, setIsError] = useState(false);
   // if text is true it returns text, if false then hello world
   const firstValue = text || "hello world";
 
   // if text is true the nit return hello world, if false then text
   const secondValue = text && "hello world";
+
+  // const changeError = () => {
+  //   setIsError(true);
+  // };
 
   console.log("first:", firstValue);
   console.log("second:", secondValue);
@@ -20,6 +24,22 @@ const ShortCircuit = () => {
       <h1>value : {secondValue}</h1> */}
       <h1>{text || "Hello"}</h1>
       {text && <h1>Kaiden Rocks</h1>}
+      <button
+        type="button"
+        className="btn"
+        onClick={() => setIsError(!isError)}
+      >
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+
+      {isError ? (
+        <p>there is an error!</p>
+      ) : (
+        <div>
+          <h2>there is no error!</h2>
+        </div>
+      )}
     </>
   );
 };
